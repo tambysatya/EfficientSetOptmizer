80cc9336eb470c4b9de469217ee6217e6df22b33
satya tamby <tambysatya@gmail.com>
2022-12-30 20:30:13 +0100

    first commit: from nadirsolver

---
+++ src/SearchRegion/Class.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving #-}
+module SearchRegion.Class where
+
+import qualified Data.Array as A
+
+type Bound = A.Array Int Double
+newtype Ideal = Ideal Bound
+newtype AntiIdeal = AntiIdeal Bound
+type GlobalBounds = (AntiIdeal, Ideal)
+
+
+data Point = Point {_ptPerf :: A.Array Int Double,
+                    _ptSol :: A.Array Int Double}
+
+newtype ProjDir = ProjDir {fromProjDir :: Int}
+	deriving (Eq, Num, Ord, A.Ix)
+instance Show ProjDir where
+    show (ProjDir p) = "proj=" ++ show p
+
+
+
+class Boundary a where
+    toBound :: a -> Bound
+
+instance Boundary Point where
+    toBound = _ptPerf
+
+instance Boundary (A.Array Int Double) where
+    toBound = id
+instance Boundary AntiIdeal where
+    toBound (AntiIdeal yU) = yU
+
+dimension :: Boundary a => a -> Int
+dimension z = snd $ A.bounds $ toBound z
+
+proj :: (Boundary a) => ProjDir -> a -> Bound -- TODO linear type ?
+proj (ProjDir k) z = A.listArray (1, dimension z - 1) [toBound z A.! i | i <- [1..dimension z], i /= k]
+
+domS :: (Boundary a, Boundary b) => a -> b -> Bool
+x `domS` y = and $ zipWith (<) (A.elems $ toBound x) (A.elems $ toBound y)
+
+domL :: (Boundary a, Boundary b) => a -> b -> Bool
+x `domL` y = and $ zipWith (<=) (A.elems $ toBound x) (A.elems $ toBound y)
+
+instance Show Point where
+    show = show . A.elems . _ptPerf
---
+++ src/SearchRegion/UB.hs
@@ -0,0 +1,162 @@
+{-# LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving #-}
+module SearchRegion.UB where
+
+import SearchRegion.Class
+import Control.Lens
+import qualified Data.Array as A
+import Data.List
+import Data.Maybe
+import Data.Function
+
+maxbound :: Double
+maxbound = 100000000
+
+{-| Wrapping some integers to avoid spending brain ticks on stupid mistakes -}
+newtype ChildDir = ChildDir {fromChildDir :: Int}
+    deriving (Show, Eq, Ord, Num, A.Ix)
+
+
+
+
+
+data UB = UB {_szU :: !Bound,
+              _szDefiningPoint :: ! (A.Array Int [Point]),
+              _szMaxProj :: (Double, ProjDir)}
+
+makeLenses ''UB
+
+
+newtype ExploredUB = ExploredUB {fromExplored :: UB }
+
+data SRUB = SRUB {_srub :: ![UB]}
+
+instance Boundary UB where
+    toBound = _szU
+instance Eq UB where
+    z == z' = fst (_szMaxProj z) == fst (_szMaxProj z')
+instance Ord UB where
+    compare = compare `on` (fst . _szMaxProj)
+
+instance Show UB where
+    show ub = show (A.elems $ _szU ub)
+
+instance Boundary ExploredUB where
+    toBound (ExploredUB ub) = toBound ub
+
+
+
+mkZone :: GlobalBounds -> UB
+mkZone (yA,yI) = UB (fmap (+1) $ toBound yA) (A.listArray (1,p) $ take p $ repeat []) (0,ProjDir 2)
+    where (1,p) = A.bounds $ toBound yA
+
+
+{-| Updates a zone z and returns the potentially
+      - Nothing (deleted by a reduction rule)
+      - The singleton [z], the zone unchanged except potentially its defining points
+      - A child
+ -}     
+
+{-
+updateZone :: GlobalBounds -> NadDir -> ExploredUB -> ProjDir -> Maybe Point -> UB -> ChildDir -> Maybe UB
+updateZone gbnds ndir zexp pdir pt ub cdir
+    | isJust pt = updateZoneJustWithRR gbnds ndir zexp pdir ub (fromJust pt) cdir
+    | otherwise = updateZoneNothing gbnds ndir zexp pdir ub cdir
+-}
+
+-- TODO strict evaluation
+updateZoneNothing :: GlobalBounds -> ExploredUB -> ProjDir -> UB -> Maybe UB
+updateZoneNothing gbnds (ExploredUB zexp) pdir  z 
+    | proj pdir z `domL` proj pdir zexp = Nothing
+    | otherwise = Just z
+    
+
+
+-- TODO strict evaluation
+updateZoneJustWithRR :: GlobalBounds -> ExploredUB -> ProjDir -> Double -> UB -> Point -> ChildDir -> Maybe UB
+updateZoneJustWithRR gbnds zexp pdir lb_l ub  pt cdir = updateZoneJust gbnds ub pt cdir
+                                 >>= applyReductionRule zexp pdir lb_l 
+updateZoneJust :: GlobalBounds -> UB -> Point -> ChildDir -> Maybe UB
+updateZoneJust gbnds ub  pt cdir 
+        | pt `domS` ub = child gbnds pt ub cdir 
+        | pt `domL` ub = Just $ updateDefiningPoints pt ub
+        | otherwise = Just ub
+ 
+
+{-| Applies the reduction rule if a point have been found y have been found after looking for 
+    improving component k by searching in direction l:
+    If child-l <= zexp-l (projection is included
+    and pt_l >= child_l 
+    then, no point in child can improve the componnent k
+ -}
+applyReductionRule ::
+           ExploredUB -- The zone
+           -> ProjDir -- The projection that have been explored
+           -> Double -- lower bound on projdir
+           -> UB
+           -> Maybe UB
+applyReductionRule zexp pdir lb_l ub 
+    | reductionRuleP zexp pdir lb_l ub = Nothing
+    | otherwise = Just ub
+   where l = fromProjDir pdir
+
+reductionRuleP (ExploredUB zexp) pdir lb_l ub
+        | proj pdir ub `domL` proj pdir zexp &&
+          lb_l  >= _szU ub A.! l = True
+        | otherwise = False
+   where l = fromProjDir pdir
+
+    
+
+
+
+{-| Generates the child *childir* from ub and pt after exploring zexp. Checks if the reduction rule applies -}
+child :: GlobalBounds 
+       -> Point -- The point that have been found (minimizing y_l over u_{-l})
+       -> UB  -- The search zone to be subdivided
+       -> ChildDir -- The direction of the subdivision (index of the child) 
+       -> Maybe UB -- The result if it stil has a componnent to explore 
+child (yA, (Ideal yI)) pt ub (ChildDir cdir) 
+      | _ptPerf pt A.! cdir == yI A.! cdir = Nothing -- Ideal contributing point found
+      | and [not $ null $  _szDefiningPoint child A.! i |  -- All unbounded components have at least one defining point
+                      i <- [1..p], 
+                      childub A.! i /= (toBound yA A.! i + 1)] 
+                = Just child
+      | otherwise = Nothing
+    where p = snd $ A.bounds childub
+          childub = _szU ub A.// [(cdir, _ptPerf pt A.! cdir)]
+          childmaxproj = (projVal yA ub $ ProjDir cdir, ProjDir cdir) --computeMaxProj yA childub  
+          childdefpts = A.array (1,p) $ (cdir,[pt]):[(i, validPts) | i <- [1..p],
+                                                                     i /= cdir, 
+                                                                     let pts = _szDefiningPoint ub A.! i
+                                                                         validPts = [pti | pti <- pts, _ptPerf pti A.! cdir < _ptPerf pt A.! cdir]]
+          child = ub & szU .~ childub
+                     & szDefiningPoint .~ childdefpts
+                     & szMaxProj .~ childmaxproj
+
+
+       
+{- Utils -}
+
+             
+computeMaxProj :: AntiIdeal -> Bound -> (Double, ProjDir)
+computeMaxProj yA ub = minimumBy (compare `on` fst) [(projVal yA ub $ ProjDir i, ProjDir i)  | i <- [1..p], ub A.! i /= (toBound yA A.! i + 1)]
+    where (_,p) = A.bounds ub
+          -- projVal is negated since we manipulates min heap
+projVal yA ub i = negate $ sum $ logBase 2 <$> zipWith (-) (A.elems $ proj i yA) (A.elems $ proj i ub)
+
+
+
+updateDefiningPoints :: Point -> UB -> UB
+updateDefiningPoints pt zone = foldr f zone $ ProjDir <$> [1..dimension zone]
+    where f pdir@(ProjDir i) acc  
+                | proj pdir pt `domS` proj pdir zone = acc & szDefiningPoint . ix i %~ (pt:) 
+                | otherwise = acc
+
+childKHasValidDefPoint (AntiIdeal yA,_) pt k z = and  [not $ null $ validPts | 
+                                    i <- [1..p],
+                                        i /= k,
+                                        _szU z A.! i /= yA A.! i,
+                                        let pts = _szDefiningPoint z A.! i
+                                            validPts = [pti | pti <- pts, _ptPerf pti A.! k < _ptPerf pt A.! k]]
+        where (_,p) = A.bounds $ _szU z
+ 
